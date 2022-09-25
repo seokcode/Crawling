dac7b55ce69c3e0ec8853bbe9ab8aeaa383da9bd
seokcode <ssd3784@gmail.com>
2022-09-25 23:43:46 +0900

    환경변수 추가

---
+++ config/environ.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Environ:
+    R3_DB_HOST = os.environ['R3_DB_HOST']
+    R3_DB_USER = os.environ['R3_DB_USER']
+    R3_DB_PASS = os.environ['R3_DB_PASS']
+    R3_DB_NAME = os.environ['R3_DB_NAME']
+
+    # reference
+    USER_AGENT = os.environ['USER_AGENT']
+    KOREA_DICT_API_KEY = os.environ['KOREA_DICT_API_KEY']
